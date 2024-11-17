88e283074619c7855b3b3bc11187c93bb0f00a65
yusif-mohey <moheyy28@gmail.com>
2024-11-17 14:25:04 +0200

    second one

---
+++ lib/core/utils/app_router.dart
@@ -0,0 +1,31 @@
+import 'package:bookly/Features/home/presentation/views/screen/book_details_view.dart';
+import 'package:bookly/Features/home/presentation/views/screen/home_view.dart';
+import 'package:bookly/Features/search/presentation/views/screen/search_view.dart';
+import 'package:go_router/go_router.dart';
+
+abstract class AppRouter {
+  static const kHomeView = '/homeView';
+  static const kBookDetailsView = '/bookDetailsView';
+  static const kSearchView = '/searchView';
+
+  static final router = GoRouter(
+    routes: [
+      GoRoute(
+        path: '/',
+        builder: (context, state) => const HomeView(),
+      ),
+      GoRoute(
+        path: kSearchView,
+        builder: (context, state) => const SearchView(),
+      ),
+      // GoRoute(
+      //   path: kHomeView,
+      //   builder: (context, state) => const HomeView(),
+      // ),
+      GoRoute(
+        path: kBookDetailsView,
+        builder: (context, state) => const BookDetailsView(),
+      ),
+    ],
+  );
+}
