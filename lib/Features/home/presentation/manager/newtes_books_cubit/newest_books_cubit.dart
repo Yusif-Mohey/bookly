b075229f034900fa22fcca5e54c256564ae5df00
yusif-mohey <moheyy28@gmail.com>
2024-11-18 23:46:32 +0200

    create newest books cubit

--- lib/Features/home/presentation/manager/newtes_books_cubit/newest_books_cubit.dart
+++ lib/Features/home/presentation/manager/newtes_books_cubit/newest_books_cubit.dart
@@ -1,8 +1,19 @@
 import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+import 'package:bookly/Features/home/domain/entities/book_entity.dart';
+import 'package:bookly/Features/home/domain/use_cases/fetch_newest_books_use_case.dart';
 
 part 'newest_books_state.dart';
 
 class NewestBooksCubit extends Cubit<NewestBooksState> {
-  NewestBooksCubit() : super(NewestBooksInitial());
+  NewestBooksCubit(this.fetchNewestBooksUseCase) : super(NewestBooksInitial());
+  final FetchNewestBooksUseCase fetchNewestBooksUseCase;
+  Future<void> featchFeatureBooksUseCase() async {
+    emit(NewestBooksLoading());
+    var result = await fetchNewestBooksUseCase.call();
+    result.fold((faliure) {
+      emit(NewestBooksFaliure(faliure.message));
+    }, (books) {
+      emit(NewestBooksSucess(books));
+    });
+  }
 }
--- lib/Features/home/presentation/manager/newtes_books_cubit/newest_books_state.dart
+++ lib/Features/home/presentation/manager/newtes_books_cubit/newest_books_state.dart
@@ -1,6 +1,19 @@
 part of 'newest_books_cubit.dart';
 
-@immutable
-sealed class NewestBooksState {}
+abstract class NewestBooksState {}
 
 final class NewestBooksInitial extends NewestBooksState {}
+
+final class NewestBooksLoading extends NewestBooksState {}
+
+final class NewestBooksSucess extends NewestBooksState {
+  final List<BookEntity> books;
+
+  NewestBooksSucess(this.books);
+}
+
+final class NewestBooksFaliure extends NewestBooksState {
+  final String errMessage;
+
+  NewestBooksFaliure(this.errMessage);
+}
