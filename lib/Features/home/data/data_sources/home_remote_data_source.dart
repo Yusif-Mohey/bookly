4e8eb1a61c0fa8be595b10b1a4958eedaac3bb7a
yusif-mohey <moheyy28@gmail.com>
2024-11-18 16:04:15 +0200

    create home remote data source

---
+++ lib/Features/home/data/data_sources/home_remote_data_source.dart
@@ -0,0 +1,37 @@
+import 'package:bookly/Features/home/data/models/book_model/book_model.dart';
+import 'package:bookly/Features/home/domain/entities/book_entity.dart';
+import 'package:bookly/core/utils/app_service.dart';
+
+abstract class HomeRemoteDataSource {
+  Future<List<BookEntity>> fetchFeatureBooks();
+  Future<List<BookEntity>> fetchNewesteBooks();
+}
+
+class HomeRemoteDataSourceImp extends HomeRemoteDataSource {
+  final AppService appService;
+
+  HomeRemoteDataSourceImp(this.appService);
+  @override
+  Future<List<BookEntity>> fetchFeatureBooks() async {
+    var data = await appService.get(
+        endPoint: 'volumes?Filtering=free-ebooks&q=programming');
+    List<BookEntity> books = getBooksList(data);
+    return books;
+  }
+
+  @override
+  Future<List<BookEntity>> fetchNewesteBooks() async {
+    var data = await appService.get(
+        endPoint: 'volumes?Filtering=free-ebooks&sort=newest&q=programming');
+    List<BookEntity> books = getBooksList(data);
+    return books;
+  }
+
+  List<BookEntity> getBooksList(Map<String, dynamic> data) {
+    List<BookEntity> books = [];
+    for (var bookMap in data['items']) {
+      books.add(BookModel.fromJson(bookMap));
+    }
+    return books;
+  }
+}
