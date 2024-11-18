596c64d2431fb327b935621924c0417fbb7bb913
yusif-mohey <moheyy28@gmail.com>
2024-11-18 23:46:20 +0200

    create feature books cubit

--- lib/Features/home/presentation/manager/feature_books_cubit/feature_books_cubit.dart
+++ lib/Features/home/presentation/manager/feature_books_cubit/feature_books_cubit.dart
@@ -1,9 +1,19 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly/Features/home/domain/entities/book_entity.dart';
-import 'package:meta/meta.dart';
+import 'package:bookly/Features/home/domain/use_cases/fetch_feature_books_use_case.dart';
 
 part 'feature_books_state.dart';
 
 class FeatureBooksCubit extends Cubit<FeatureBooksState> {
-  FeatureBooksCubit() : super(FeatureBooksInitial());
+  FeatureBooksCubit(this.featureBooksUseCase) : super(FeatureBooksInitial());
+  final FetchFeatureBooksUseCase featureBooksUseCase;
+  Future<void> featchFeatureBooksUseCase() async {
+    emit(FeatureBooksLoading());
+    var result = await featureBooksUseCase.call();
+    result.fold((faliure) {
+      emit(FeatureBooksFaluire(faliure.message));
+    }, (books) {
+      emit(FeatureBooksSucess(books));
+    });
+  }
 }
--- lib/Features/home/presentation/manager/feature_books_cubit/feature_books_state.dart
+++ lib/Features/home/presentation/manager/feature_books_cubit/feature_books_state.dart
@@ -1,7 +1,6 @@
 part of 'feature_books_cubit.dart';
 
-@immutable
-sealed class FeatureBooksState {}
+abstract class FeatureBooksState {}
 
 final class FeatureBooksInitial extends FeatureBooksState {}
 
